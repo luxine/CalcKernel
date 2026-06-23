5c3de530c50f2032aa5b8cce22372caf8e300229
luke <olianna.luxine@foxmail.com>
2026-06-23 19:29:12 +0800

    feat: activate intkernel vscode language features

---
+++ ik-vscode-plugin/src/extension.ts
@@ -0,0 +1,12 @@
+import * as vscode from "vscode";
+import { registerCompletions } from "./completions";
+import { registerDiagnostics } from "./diagnostics";
+
+export function activate(context: vscode.ExtensionContext): void {
+  registerDiagnostics(context);
+  registerCompletions(context);
+}
+
+export function deactivate(): void {
+  // VSCode disposes subscriptions registered on the extension context.
+}
